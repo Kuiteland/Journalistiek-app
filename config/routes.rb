a57de2c39b9bb8ede289d31c60ad89d1791d11ec
Banno <banno@live.nl>
2014-01-22 16:20:19 +0100

    project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 JourDat::Application.routes.draw do
+  resources :users
+  root 'main#home'
+  match 'create', to: 'users#new', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
