f98506f3fe99565717ed2b02194862b93b9fee71
Banno <banno@live.nl>
2014-01-26 22:19:10 +0100

    tweaks and precompile

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,7 +1,7 @@
 class SubjectsController < ApplicationController
 
   def index
-    @subjects = Subject.paginate(page: params[:page], :per_page => 1)
+    @subjects = Subject.paginate(page: params[:page], :per_page => 30)
   end
 
   def new
@@ -16,7 +16,7 @@ class SubjectsController < ApplicationController
     @subject = Subject.new(subject_params)
     if @subject.save
       flash[:success] = "Nieuw onderwerp aangemaakt!"
-      redirect_to root_path
+      redirect_to subjects_url
     else
       render 'new'
     end
@@ -39,7 +39,7 @@ class SubjectsController < ApplicationController
   def destroy
     Subject.find(params[:id]).destroy
     flash[:success] = "Subject deleted."
-    redirect_to users_url
+    redirect_to subjects_url
   end
 
 
